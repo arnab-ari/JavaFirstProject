acfe42815130683b425b2221a3643c6a067969c3
Arnab Ari <arnab.ari.ndp@gmail.com>
2020-04-10 02:30:52 +0530

    CheckEven class added and changes made to the pom file

---
+++ src/main/java/com/sapient/week1/CheckEven.java
@@ -0,0 +1,14 @@
+package com.sapient.week1;
+
+public class CheckEven 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is app to check even numbers." );
+    }
+     public static Boolean Check( int n )
+    {
+         return (n%2==0);
+    }
+
+}
