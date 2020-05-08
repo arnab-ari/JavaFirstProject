dcef4985861221522c0b5193c21280178c0f189f
Arnab Ari <arnab.ari.ndp@gmail.com>
2020-05-09 01:19:52 +0530

    Figure to words file added

---
+++ week3/FigureToWords/src/com/sapient/FigureToWords/FigureToWordRunner.java
@@ -0,0 +1,11 @@
+package com.sapient.FigureToWords;
+
+public class FigureToWordRunner {
+	public static void main(String[] args) {
+		long number = 718291236548l;
+
+		System.out.println(FigureToWords.fig_to_word(number));
+
+	}
+
+}
